43f67b1fc0522da172e37877050b22a9f5fada86
Norman Sobczyk <wgwcompany@duck.com>
2025-06-03 12:19:47 +0000

    update

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,10 +1,21 @@
 import Lock from "styles/icons/lock";
+import GithubIcon from "styles/icons/github";
 
 export default function Footer() {
   return (
     <footer>
       <h3>Your secure messenger for creating one-time encrypted messages.</h3>
       <Lock />
+      <aside className="github">
+        <div className="box">
+          <div>
+            <a href="https://github.com/niezle-ziolko/cs50x-final-project" target="_blank" rel="noreferrer">
+              <p>View on GitHub</p>
+              <GithubIcon />
+            </a>
+          </div>
+        </div>
+      </aside>
     </footer>
   );
 };
