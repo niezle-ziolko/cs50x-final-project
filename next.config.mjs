108309f4f51d27c0e666c50908c3b0a6487090c4
_niezle_ziolko <120026902+niezle-ziolko@users.noreply.github.com>
2025-06-11 09:22:44 +0000

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import { loadEnv } from './src/lib/env.js';
-import withPreact from '@preact/next';
 
 loadEnv();
 initOpenNextCloudflareForDev();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
-export default withPreact(nextConfig);
+export default nextConfig;
